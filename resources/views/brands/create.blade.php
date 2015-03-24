caf4aba85b677f83e2b0095315ad7cf8fdaf0699
Ruchir <ruchir.kakkad@gmail.com>
2015-03-24 13:53:53 +0530

    brands module with design

--- resources/views/brands/create.blade.php
+++ resources/views/brands/create.blade.php
@@ -1,13 +1,31 @@
 @extends('template.master')
 
 @section('content')
+<section class="content">
+	<div class="row">
+		<!-- left column -->
+		<div class="col-md-6">
+			<!-- general form elements -->
+			<div class="box box-primary">
+				<div class="box-header">
+					<h3 class="box-title">Create new Brand</h3>
+				</div><!-- /.box-header -->
+				<!-- form start -->
+				<form role="form" action="{{ url('brands/store') }}" method="post">
+					<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
+					<div class="box-body">
+						<div class="form-group">
+							<label for="exampleInputEmail1">Name</label>
+							<input type="text" class="form-control"  name="name" placeholder="Brand Name">                      
+						</div>                    
+					</div><!-- /.box-body -->
 
-
-
-<form action="{{ url('brands/store') }}" method="post">
-	<input type="hidden" name="_token" value="{{{ csrf_token() }}}" />
-	<input type="text" name="name" placeholder="Brand Name">
-	<input type="submit" name="submit" value="Create">
-</form>
-
+					<div class="box-footer">
+						<button type="submit" class="btn btn-primary">Submit</button>
+					</div>
+				</form>
+			</div><!-- /.box -->
+		</div>
+	</div>
+</section>
 @stop
--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -1,27 +1,40 @@
 @extends('template.master')
 
 @section('content')
-<h2>Brands</h2>
 
-<div>
-	<a href="{{ url('brands/create') }}">Create New</a>
-</div>
-<table border='1'>
-	<tr>
-		<th>Id</th>
-		<th>Name</th>
-		<th>Products Count</th>
-	</tr>
+<section>
+	<div class="row">
+		<div class="col-xs-12">
+			<div class="box">
+				<div class="box-header">
+					<h3 class="box-title">Brands</h3>
+					<div class="box-tools">
+						<div class="input-group">							
+							<div class="input-group-btn">
+								<button class="btn btn-sm btn-default"><a href="{{ url('brands/create') }}">Create New</a></button>
+							</div>
+						</div>
+					</div>
+				</div><!-- /.box-header -->
+				<div class="box-body table-responsive no-padding">
+					<table class="table table-hover">
+						<tr>
+							<th>ID</th>
+							<th>Name</th>
+							<th>Products Count</th>                      
+						</tr>
+						@foreach($brands as $brand)
+						<tr>
+							<td>{{ $brand->id }}</td>
+							<td>{{ $brand->name }}</td>
+							<td><span class="label label-success">0</span></td>
+						</tr>
+						@endforeach                    
+					</table>
+				</div><!-- /.box-body -->
+			</div><!-- /.box -->
+		</div>
+	</div>
 
-	@foreach($brands as $brand)
-	<tr>
-		<td>{{ $brand->id }}</td>
-		<td>{{ $brand->name }}</td>
-		<td>0</td>
-	</tr>
-	@endforeach
-
-
-</table>
-
+</section>
 @stop
